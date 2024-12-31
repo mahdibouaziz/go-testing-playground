9958e1474740792093f8a5257d5b4c8720d734cc
Mahdi Bouaziz <mahdiibouaziz@gmail.com>
2024-12-31 16:47:24 +0100

    render a simple page

--- webapp/cmd/web/handlers.go
+++ webapp/cmd/web/handlers.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home page")
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]interface{}
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// parse the template from disk
+	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	// execute the template, passing it data if any
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- webapp/go.mod
+++ webapp/go.mod
@@ -2,4 +2,4 @@ module github.com/mahdibouaziz/go-testing-playground/webapp
 
 go 1.23.4
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require github.com/go-chi/chi/v5 v5.2.0
