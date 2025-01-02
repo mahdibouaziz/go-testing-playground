e41d1d2cdc1fe00003491b78e247ff28739990e3
Mahdi Bouaziz <mahdiibouaziz@gmail.com>
2025-01-02 17:51:07 +0100

    feat: add db connection

--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 
 	"github.com/alexedwards/scs/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type application struct {
 	Session *scs.SessionManager
+	DSN     string
+	DB      *pgxpool.Pool
 }
 
 func main() {
@@ -17,11 +20,21 @@ func main() {
 		Session: getSession(),
 	}
 
+	// Connect to the db
+	app.DSN = "postgres://postgres:postgres@localhost:5432/users"
+	// app.DSN = os.Getenv("DATABASE_URL") // TODO - change this later
+	db, err := app.connectToDB()
+	if err != nil {
+		log.Fatalf("Unable to connect to the database: %v", err)
+	}
+	defer db.Close() // Ensure the database connection is closed on exit
+	app.DB = db
+
 	// print out a message
 	log.Println("Starting server on port 8080")
 
 	// start the server
-	err := http.ListenAndServe(":8080", app.routes())
+	err = http.ListenAndServe(":8080", app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
