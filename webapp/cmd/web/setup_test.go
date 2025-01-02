8d5ee2d4c498f201c2e0a8558b046af25cba33ae
Mahdi Bouaziz <mahdiibouaziz@gmail.com>
2025-01-02 19:23:58 +0100

    test login

--- webapp/cmd/web/setup_test.go
+++ webapp/cmd/web/setup_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+
+	"github.com/mahdibouaziz/go-testing-playground/webapp/pkg/db"
 )
 
 var app application
@@ -11,6 +14,16 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
+
+	// Connect to the db
+	app.DSN = "postgres://postgres:postgres@localhost:5432/users"
+	// app.DSN = os.Getenv("DATABASE_URL") // TODO - change this later if you need a dynamic db (for this example it is just a playground)
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatalf("Unable to connect to the database: %v", err)
+	}
+	defer conn.Close()
+	app.DB = db.PostgresConn{DB: conn}
 
 	os.Exit(m.Run())
 }
